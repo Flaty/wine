b8672fed797d4854f26bf0845a3ce4b93a4c753c
Flaty <78325079+Flaty@users.noreply.github.com>
2024-10-18 00:00:48 +0300

    10 урок

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+
+def get_year_declension(years):
+    if years % 10 == 1 and years % 100 != 11:
+        return "год"
+    elif 2 <= years % 10 <= 4 and not (12 <= years % 100 <= 14):
+        return "года"
+    else:
+        return "лет"
+
+
+year = datetime.date.today().year - 1920
+year_declension = get_year_declension(year)
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(
+    years=year,
+    years_declension=year_declension
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
+print(1)
